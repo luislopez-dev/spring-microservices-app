52db83d2bba4b3a6934f5a0b6a4d694d4002c5f4
Luis Lopez <luis.dev.master@gmail.com>
2023-08-17 00:40:51 -0600

    Adding annotations, methods and fields to Order entity

--- booking-service/src/main/java/com/example/demo/Entity/Order.java
+++ booking-service/src/main/java/com/example/demo/Entity/Order.java
@@ -1,4 +1,21 @@
 package com.example.demo.Entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Table(name = "order")
+@Data
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String orderNo;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems;
 }
