ac8b5a4f5b29077ca8530c45ad6ea62f2870dc7f
Luis Lopez <luis.dev.master@gmail.com>
2023-08-16 22:38:14 -0600

    Adding getStockAvailable method in StockController

--- stock-service/src/main/java/com/example/demo/Controller/StockController.java
+++ stock-service/src/main/java/com/example/demo/Controller/StockController.java
@@ -1,4 +1,26 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Entities.Stock;
+import com.example.demo.Repository.StockRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/stock")
 public class StockController {
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    public boolean stockAvailable(@PathVariable String code) {
+        Optional<Stock> stock = stockRepository.findByCode(code);
+
+        stock.orElseThrow(() -> new RuntimeException("Can not find product" + code));
+
+        return stock.get().getQuantity() > 0;
+    }
 }
