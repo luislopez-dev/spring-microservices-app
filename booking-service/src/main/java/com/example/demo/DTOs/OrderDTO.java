b92120e50e02530f1c492108174397e51aa391f0
Luis Lopez <luis.dev.master@gmail.com>
2023-08-17 00:44:53 -0600

    Adding properties to OrderDTO class

--- booking-service/src/main/java/com/example/demo/DTOs/OrderDTO.java
+++ booking-service/src/main/java/com/example/demo/DTOs/OrderDTO.java
@@ -1,4 +1,16 @@
 package com.example.demo.DTOs;
 
+import com.example.demo.Entity.OrderItem;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class OrderDTO {
+
+    private List<OrderItem> orderItems;
 }
