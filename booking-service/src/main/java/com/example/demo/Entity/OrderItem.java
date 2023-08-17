aaa1446bf7a64e29813275770b6a55f4361e6b1d
Luis Lopez <luis.dev.master@gmail.com>
2023-08-17 00:28:25 -0600

    Creating methods and filds for Orderitem entity

--- booking-service/src/main/java/com/example/demo/Entity/OrderItem.java
+++ booking-service/src/main/java/com/example/demo/Entity/OrderItem.java
@@ -1,4 +1,22 @@
 package com.example.demo.Entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "order_item")
+@Data
 public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String code;
+
+    private BigDecimal price;
+
+    private Integer orderQty;
 }
